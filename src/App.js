70c9d0df7bef71e8098a749efc4d2544ba543266
Oraekiprincess <114993346+Oraekiprincess@users.noreply.github.com>
2024-03-30 18:21:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
+// src/App.js
+
+import React from 'react';
+import Notepad from './Notepad';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Notepad</h1>
+        <Notepad />
+      </header>
+    </div>
+  );
+}
+
+export default App;
 
